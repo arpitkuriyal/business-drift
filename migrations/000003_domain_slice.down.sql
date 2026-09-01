3a32611e86f9433028df0d30d0e5e479957d9e50
Arpit Kuriyal <arpitkuriyal2002@gmail.com>
2026-09-01 09:22:00 +0530

    add findings schema

---
+++ migrations/000003_domain_slice.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS finding_evidence;
+DROP TABLE IF EXISTS findings;
+DROP TABLE IF EXISTS customer_facts;
+DROP TABLE IF EXISTS customer_identities;
+DROP TABLE IF EXISTS canonical_customers;
